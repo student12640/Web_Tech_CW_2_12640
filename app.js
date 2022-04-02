e2195c2e95e37bea8c39affa484429047c3b85c6
Otadjanov Akhror <wiut00012640@gmail.com>
2022-04-02 12:52:13 +0500

    edit and app added

--- app.js
+++ app.js
@@ -57,3 +57,73 @@ app.post("/add", (req, res) => {
     });
   }
 });
+app.get("/:id/delete", (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile("./data/todos.json", (err, data) => {
+    if (err) throw err;
+
+    const todos = JSON.parse(data);
+
+    const filterTodos = todos.filter((todo) => todo.id != id);
+
+    fs.writeFile("./data/todos.json", JSON.stringify(filterTodos), (err) => {
+      if (err) throw err;
+
+      res.render("home", { todos: filterTodos, deleted: true });
+    });
+  });
+});
+
+app.get("/:id/update", (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile("./data/todos.json", (err, data) => {
+    if (err) throw err;
+
+    const todos = JSON.parse(data);
+    const todo = todos.filter((todo) => todo.id == id)[0];
+    const todoIndex = todos.indexOf(todo);
+    const todoSpliced = todos.splice(todoIndex, 1)[0];
+    todoSpliced.done = true;
+    todos.push(todoSpliced);
+
+    fs.writeFile("./data/todos.json", JSON.stringify(todos), (err) => {
+      if (err) throw err;
+
+      res.render("home", { todos: todos });
+    });
+  });
+});
+
+app.post("/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const todo = { ...req.body };
+
+  fs.readFile("./data/todos.json", (err, data) => {
+    const todos = JSON.parse(data.toString());
+    const selectedIndex = todos.findIndex((todo) => todo.id === id);
+    todos[selectedIndex] = {
+      id,
+      description: todo.description,
+      done: todos[selectedIndex].done,
+    };
+    if (err) throw err;
+
+    fs.writeFile("./data/todos.json", JSON.stringify(todos), (err) => {
+      if (err) throw err;
+
+      res.render("home", { success: true, todos });
+    });
+  });
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log(`This app is running ${PORT}`);
+});
+
+function id() {
+  return "_" + Math.random().toString(36).substr(2, 9);
+}
